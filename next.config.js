5c10640e13576764218e28110833dbc6d5aced42
Cody Robinson <crobinson@skillpath.com>
2021-12-20 12:52:15 -0600

    build

--- next.config.js
+++ next.config.js
@@ -6,4 +6,5 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
     // Use the CDN in production and localhost for development.
     assetPrefix: isProd ? '/undo2/' : '',
+    basePath: '/docs'
 };
