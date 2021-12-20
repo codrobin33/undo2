300b02fda15a005455cb1d11c74b4de35b42606c
Cody Robinson <crobinson@skillpath.com>
2021-12-20 12:41:52 -0600

    pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+console.log('NODE ENV: ', process.env.NODE_ENV, process.env.NODE_ENV === 'production')
+
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? '/gi-joe/' : '',
+};
