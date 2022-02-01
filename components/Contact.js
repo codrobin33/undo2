f69c021c73b1fa598b898b09d408391631d3702f
Cody Robinson <cody@codybayrobinson.com>
2022-02-01 09:09:27 -0600

    build

--- components/Contact.js
+++ components/Contact.js
@@ -1,20 +1,20 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
-import { lazy, Suspense } from 'react';
+//import { lazy, Suspense } from 'react';
 
-const MapComponent = lazy(() => import('./Map'));
+//const MapComponent = lazy(() => import('./Map'));
 
 const Contact = () => {
 
     return (
         <div className="bg-white" id="contact">
-            <div suppressHydrationWarning={true}>
+            {/* <div suppressHydrationWarning={true}>
                 {process.browser &&
                     <Suspense fallback={<>loading...</>}>
                         <MapComponent />
                     </Suspense>
                 }
-            </div>
+            </div> */}
 
             <div className=" py-16 px-4 sm:px-6 lg:px-8">
                 <div className="md:max-w-none md:grid md:grid-cols-2 md:gap-8 text-center">
