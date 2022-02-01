be754b31e87faf82fc502268cacc1bb8928ffe3f
Cody Robinson <cody@codybayrobinson.com>
2022-02-01 15:01:19 -0600

    build

--- components/ImageHeader.js
+++ components/ImageHeader.js
@@ -5,7 +5,7 @@ const ImageHeader = () => {
     return (
         <div className="relative bg-indigo-800">
             <div className="absolute inset-0">
-                <div className="relative w-full h-full bg-gray-900/10 bg-gradient-to-b from-gray-900/50 via-gray-900/20 to-gray-900 z-10" />
+                <div className="relative w-full h-full bg-gray-800/10 bg-gradient-to-b from-gray-800/50 via-gray-800/20 to-gray-800 z-10" />
 
                 <div className="h-full w-full">
                     <Image
@@ -15,12 +15,11 @@ const ImageHeader = () => {
                         layout="fill"
                     />
                 </div>
-                <div className="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
             </div>
 
             <div className="relative py-24 px-4 sm:py-32 sm:px-6 lg:px-8 z-10">
-                <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl mt-14">How Does Radiofrequency Body Contouring Work?</h1>
-                <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+                <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl mt-14 drop-shadow-xl">How Does Radiofrequency Body Contouring Work?</h1>
+                <p className="mt-6 text-xl text-white drop-shadow-xl max-w-3xl">
                     This is a non-invasive technique that uses heat and RF energy to shape your body by getting rid of undesirable fat cells. The technology also smooths your skin where cellulite or sagging may exist.
                 </p>
             </div>
--- components/ServicesHeader.js
+++ components/ServicesHeader.js
@@ -6,7 +6,7 @@ const ServicesHeader = () => {
         <div className="relative bg-indigo-800">
             <div className="pt-24 lg:pb-20">
                 <div className="absolute top-0 w-full h-full">
-                    <div className="relative w-full h-full bg-gray-900/10 bg-gradient-to-b from-gray-900/50 via-gray-900/20 to-gray-900 z-10" />
+                    <div className="relative w-full h-full bg-gray-800/10 bg-gradient-to-b from-gray-800/50 via-gray-800/20 to-gray-800 z-10" />
 
                     <div className="h-full w-full">
                         <Image
@@ -18,8 +18,8 @@ const ServicesHeader = () => {
                     </div>
                 </div>
                 <div className="relative py-24 px-4 sm:py-32 sm:px-6 lg:px-8 z-10">
-                    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">All the ways we can help you.</h1>
-                    <p className="mt-6 text-xl text-gray-50 max-w-3xl">
+                    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl drop-shadow-xl">All the ways we can help you.</h1>
+                    <p className="mt-6 text-xl text-gray-50 max-w-3xl drop-shadow-xl">
                         We have wordclass equipment, staff and products to facilitate any skincare need you may have. Let us help you complete your beauty journey!
                     </p>
                 </div>
