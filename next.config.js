30193ad4491cc26734e1a41a34a0b5afffd977a4
Cody Robinson <cody@codybayrobinson.com>
2022-02-03 11:11:15 -0600

    build variables

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? '/' : '',
-    basePath: isProd ? '/' : '',
     env: {
         img_url: isProd ? '/' : ''
     },
